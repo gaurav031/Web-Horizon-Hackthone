563abfda1d170f2c7ad789514aa4944b51537b55
mantu <mantukumar87586299@gmail.com>
2024-09-27 10:13:30 +0530

    react-router-dom configure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
+// src/App.js
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
 function App() {
-
-
   return (
-    <h1 className="text-3xl font-bold text-blue-400 ">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<>i am Home</>} />
+        <Route path="/about" element={<>i am About</>} />
+        <Route path="*" element={<>i am nothing</>} /> {/* Catch-all route */}
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
