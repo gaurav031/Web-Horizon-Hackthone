5a3af75f37a19a40551f7b54668043ee6ac5ada8
mantu <mantukumar87586299@gmail.com>
2024-09-27 15:38:31 +0530

    chenge done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,13 +6,6 @@ const Navbar = () => {
       <div className="w-full max-w-6xl flex items-center justify-between px-4">
         {/* Logo */}
         <Link to="/" className="no-underline">
-
-          <span className="font-semibold text-2xl text-black">lamabooking</span>
-        </Link>
-
-        {/* Right-side buttons */}
-        <div className="hidden md:flex items-center space-x-4"> {/* Hidden on mobile */}
-
           <img src="https://i.im.ge/2024/09/27/kdlet6.logo-removebg-preview.png" alt="logo" className="w-34 h-12" />
         </Link>
 
@@ -26,27 +19,12 @@ const Navbar = () => {
             Customer Service
           </Link>
           <img src="https://i.im.ge/2024/09/27/kdlCkq.profile.jpeg" alt="Profile Photo" className="w-12 h-12 rounded-full" />
- main
           <Link to={"/login"} className="px-4 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 transition-all duration-300">
             Sign in / Register
           </Link>
 
-          
-          
-        </div>
 
-        {/* Hamburger Icon for Mobile */}
-        <div className="md:hidden flex items-center">
-          <button className="focus:outline-none">
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
-            </svg>
-          </button>
+
         </div>
       </div>
     </div>
