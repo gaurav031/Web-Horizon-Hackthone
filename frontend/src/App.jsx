dce65471127e991f0d3d3449d62f7fdeedd27030
mantu <mantukumar87586299@gmail.com>
2024-09-27 10:37:34 +0530

    folder structure ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,24 +4,8 @@ import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
 
-      <Routes>
-        <Route path="/" element={<>i am Home</>} />
-        <Route path="/about" element={<>i am About</>} />
-        <Route path="*" element={<>i am nothing</>} /> {/* Catch-all route */}
-      </Routes>
-    </div>
+    <div> i am mantu kumar morya</div>
   );
 }
 
---
+++ frontend/src/pages/HotelList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HotelList = () => {
+    return (
+        <div>
+            HotelList
+        </div>
+    )
+}
+
+export default HotelList
